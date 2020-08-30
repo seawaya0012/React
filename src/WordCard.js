f4916d19ade93ff230e95849ae6b81ed72556ae8
HAFIS LUEBAEKASENG (ฮาฟิซ ลือแบกาเส็ง) <6010110420@email.psu.ac.th>
2020-08-30 23:41:46 +0700

    Part 5

--- src/WordCard.js
+++ src/WordCard.js
@@ -2,10 +2,15 @@ import React from "react";
 import CharacterCard from "./CharacterCard";
 
 export default function Wordcard(props) {
+    const activationHandler = c => {
+        console.log(`${c} has been activated.`)
+    }
     return(
         <div>
             {
-                Array.from(props.value).map((a, i) =>  <CharacterCard value={a} key={i}/>)
+                Array.from(props.value).map((c, i) => 
+                    <CharacterCard value={c} key={i} activationHandler={activationHandler}/>
+                )
             }
         </div>
     )
